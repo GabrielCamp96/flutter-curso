4573ab0d94c4c9a58ee44037ba48474a6371e1b3
Gabriel Soares Campanário <gabriel.camp96@hotmail.com>
2019-11-18 23:14:57 -0300

    Layout e calculadora funcional, falta ajustes na mensagem de resultado

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 void main() => runApp(MaterialApp(
       title: "Calculadora IMC",
       home: Home(),
-      color: Colors.green,
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        accentColor: Colors.green,
+      ),
     ));
 
 class Home extends StatefulWidget {
@@ -12,6 +18,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double _peso = 0;
+  double _altura = 0;
+  double _resultado = 0;
+  String _mensagem = "Informe seus dados!";
+  TextEditingController _imcAltura = TextEditingController();
+  TextEditingController _imcPeso = TextEditingController();
+
+  void calcularImc(double peso, double altura) {
+    _resultado = double.parse((peso / pow(altura, 2)).toStringAsFixed(1));
+    debugPrint("$_resultado");
+    if (_resultado < 18.5) _mensagem = "IMC $_resultado: Magreza";
+    if (_resultado > 18.5 && _resultado < 25)
+      _mensagem = "IMC $_resultado: Normal";
+    if (_resultado >= 25 && _resultado < 30)
+      _mensagem = "IMC $_resultado: Sobrepeso";
+    if (_resultado > 30 && _resultado < 40)
+      _mensagem = "IMC $_resultado: Obesidade";
+    if (_resultado >= 40) _mensagem = "IMC $_resultado: Obesidade grave";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +49,68 @@ class _HomeState extends State<Home> {
             icon: Icon(
               Icons.replay,
             ),
-            onPressed: () {},
           ),
         ],
       ),
       body: Column(
         children: <Widget>[
-          Icon(
-            Icons.person_outline,
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Icon(
+                Icons.person_outline,
+                color: Colors.green[300],
+                size: 120,
+              ),
+            ),
+          ]),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+            child: TextFormField(
+              controller: _imcPeso,
+              decoration: InputDecoration.collapsed(
+                  hintText: 'Peso (kg)',
+                  hintStyle: TextStyle(color: Colors.green[300])),
+              style: TextStyle(color: Colors.green[300]),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+            child: TextFormField(
+              controller: _imcAltura,
+
+              decoration: InputDecoration.collapsed(
+                  hintText: 'Altura (m)',
+                  hintStyle: TextStyle(color: Colors.green[300])),
+              style: TextStyle(color: Colors.green[300]),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    width: 390.0,
+                    child: RaisedButton(
+                      child: Text("Calcular"),
+                      color: Colors.green[300],
+                      onPressed: () {
+                        _peso = double.parse(_imcPeso.text);
+                        _altura = double.parse(_imcAltura.text);
+                        calcularImc(_peso, _altura);
+                      },
+                      splashColor: Colors.green[300],
+                      textColor: Colors.white,
+                    ),
+                  )),
+            ],
+          ),
+          Text(
+            "${_resultado == 0 ? "Informe seus dados" : "$_mensagem"}",
+            style: TextStyle(
+              color: Colors.green[300],
+            ),
           ),
         ],
       ),
